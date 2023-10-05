7f5071ebb5ab672916b384672e8015a2f62180db
davirqch8800 <davidrqch8800@gmail.com>
2023-10-04 22:07:32 -0500

    adding the headers in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContractTypeController;
+use App\Http\Controllers\MediaContactController;
+use App\Http\Controllers\ProfessorController;
+use App\Http\Controllers\AcademicalWorkExperienceController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
-Route::resource('contract_types', ContractTypeController::class);
-Route::resource('media_contacts', MediaContactController::class);
-Route::resource('professors', ProfessorController::class);
+// Route::resource('contract_types', ContractTypeController::class);
+// Route::resource('media_contacts', MediaContactController::class);
+// Route::resource('professors', ProfessorController::class);
 Route::resource('academical_work_experiences', AcademicalWorkExperienceController::class);
